30d9c9930eec0c9d2e15cff931570dc37f4e1a4d
Reymark Torres <reymark.torres08.dev@gmail.com>
2020-01-07 10:33:22 +0800

    #4 > List Rendering

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        product: 'Socks',
-        image: './assets/vmSocks-green-onWhite.jpg',
-        inStock: true
+  el: '#app',
+  data: {
+    product: 'Socks',
+    image: './assets/vmSocks-green-onWhite.jpg',
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+    {
+      variantId: 2234,
+      variantColor: "green"
+    },
+    {
+      variantId: 2235,
+      variantColor: "blue"
     }
+    ]
+  }
 });
